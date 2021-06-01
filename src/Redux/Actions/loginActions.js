43933ccecac1c4ec320fd02f313b5c2dc17775cb
Nukareddy Sivateja Reddy <sivatejareddy.nukareddy@gmail.com>
2021-06-01 14:37:52 +0530

    Pushing the reducer

---
+++ src/Redux/Actions/loginActions.js
@@ -0,0 +1,22 @@
+import { ActionTypes } from "../constants/ActionTypes";
+
+export const setuserLoggedIn = (user) => {
+  return {
+    type: ActionTypes.USER_LOGGEDIN,
+    payload: user,
+  };
+};
+
+export const setAccessToken = (token) => {
+  return {
+    type: ActionTypes.ACCESS_TOKEN,
+    payload: token,
+  };
+};
+
+export const setAuthorization = (authorization) => {
+  return {
+    type: ActionTypes.AUTHORIZATION,
+    payload: authorization,
+  };
+};
