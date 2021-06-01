80fa5e432933d85a4fd837ad3688ef71d0b4795f
Nukareddy Sivateja Reddy <sivatejareddy.nukareddy@gmail.com>
2021-06-01 14:40:08 +0530

    Action type name changed

--- src/Redux/Reducers/LoaderReducer.js
+++ src/Redux/Reducers/LoaderReducer.js
@@ -1,4 +1,4 @@
-import { ActionTypes } from "./../constants/ActionTypes";
+import { ActionTypes } from "../constants/action-types";
 
 export const LoaderReducer = (state={}, {type, payload}) => {
     switch (type) {
--- src/Redux/Reducers/LoginReducer.js
+++ src/Redux/Reducers/LoginReducer.js
@@ -1,4 +1,4 @@
-import { ActionTypes } from "./../constants/ActionTypes";
+import { ActionTypes } from "../constants/action-types";
 
 export const UserLoginReducer = (state = {}, { type, payload }) => {
   switch (type) {
